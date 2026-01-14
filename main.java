5ff92dbf1ebfad9fe4461093d9a23d1ad5abe27d
gemma-ml <martinezlgemma@gmail.com>
2026-01-14 19:19:58 +0100

    Test

--- main.java
+++ main.java
@@ -1,9 +1,43 @@
+package Ejercicio3_Pedidos;
+
 public class main {
+
+    public static void  ImprimirEstado(EstadoPedido pedido){
+        switch (pedido) {
+            case PENDIENTE:
+                System.out.println("Tu pedido esta " + pedido);
+                break;
+            case EN_PROCESO:
+                System.out.println("Tu pedido esta " + pedido);
+                break;
+            case ENVIADO:
+                System.out.println("Tu pedido esta " + pedido);
+                break;
+            case ENTREGADO:
+                System.out.println("Tu pedido esta " + pedido);
+                break;
+            case CANCELADO:
+                System.out.println("Tu pedido esta " + pedido);
+                break;
+            default:
+                System.out.println("Tu pedido esta perdido");
+                break;
+        }
+    }
     public static void main(String[] args) {
-        //Inmobiliario pablo = Inmobiliario.PISO;
-        Color verde = Color.VERDE;
-        System.out.println(verde.getCodigo() + " " + verde.getNum());
-        
-        
-    }  
+        EstadoPedido a = EstadoPedido.PENDIENTE;
+        EstadoPedido b = EstadoPedido.PENDIENTE;
+        EstadoPedido c = EstadoPedido.PENDIENTE;
+
+        System.out.println("Tus pedidos estan pendientes");
+        a = EstadoPedido.EN_PROCESO;
+        b = EstadoPedido.CANCELADO;
+        c = EstadoPedido.ENTREGADO;
+
+        ImprimirEstado(a);
+        ImprimirEstado(b);
+        ImprimirEstado(c);
+
+
+    }
 }
