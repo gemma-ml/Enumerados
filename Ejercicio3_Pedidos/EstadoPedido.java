c638685af636e7f603d02ff4ee3bd3ab4eed2a1d
yuniormachado-sys <yunior.machado@doc.medac.es>
2026-01-14 17:58:41 +0100

    Ejercicio3

---
+++ Ejercicio3_Pedidos/EstadoPedido.java
@@ -0,0 +1,18 @@
+package Ejercicio3_Pedidos;
+
+public enum EstadoPedido {
+    /*Crea un enum EstadoPedido con valores: PENDIENTE, 
+    EN_PROCESO, ENVIADO, ENTREGADO, CANCELADO.
+
+    Escribe un método que reciba un EstadoPedido y 
+    muestre un mensaje distinto según el estado.
+
+    Simula varios pedidos cambiando su estado 
+    y mostrando el mensaje correspondiente. 
+*/
+    PENDIENTE,
+    EN_PROCESO,
+    ENVIADO,
+    ENTREGADO,
+    CANCELADO;
+}
