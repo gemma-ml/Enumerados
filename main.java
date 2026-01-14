e3eeab34acd86c17c6deefe064dd0da6e41ed750
yuniormachado-sys <yunior.machado@doc.medac.es>
2026-01-14 16:25:23 +0100

    Enumerado Simple

---
+++ main.java
@@ -0,0 +1,12 @@
+public class main {
+    public enum Inmobiliario{
+        PISO,
+        GARAJE,
+        MANSION
+    }
+    public static void main(String[] args) {
+        Inmobiliario pablo = Inmobiliario.PISO;
+        System.out.println(pablo);
+        
+    }  
+}
